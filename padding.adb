513f03536d94fd7fe574365195985733b189d3b3
Yannick Moy <moy@adacore.com>
2018-04-23 23:03:28 +0200

    Modified version without precondition, and nicer contracts

--- padding.adb
+++ padding.adb
@@ -2,12 +2,9 @@ package body Padding with SPARK_Mode is
 
    function Left_Pad (S : String; Pad_Char : Character; Len : Natural)
                       return String
-   is
-   begin
-      -- (Len - S'Length) is the number of required padding chars. We create a
-      -- string of that length with only Pad_Chars, and then concatenate this
-      -- with S.
-      return (1 .. Len - S'Length => Pad_Char) & S;
-   end Left_Pad;
+   -- (Len - S'Length) is the number of required padding chars. We create a
+   -- string of that length with only Pad_Chars, and then concatenate this
+   -- with S.
+   is ((1 .. Len - S'Length => Pad_Char) & S);
 
 end Padding;
--- padding.ads
+++ padding.ads
@@ -1,23 +1,24 @@
 package Padding with SPARK_Mode is
 
    --  add [Pad_Char] characters to the left of the string [S] so that its
-   --  length becomes [Len]
+   --  length becomes [Len] if greater than [S'Length]
    function Left_Pad (S : String; Pad_Char : Character; Len : Natural)
                       return String
-   --  This precondition seems to be needed to express the contract; The
-   --  result length should be just one less than maximal string size.
-     with Pre => Len < Natural'Last,
-   --  express the specification by cases
-     Contract_Cases =>
-   --  if the string is shorter than the desired length ...
-       (Len > S'Length =>
-   --     the "tail" of the result is equal to [S]
-            Left_Pad'Result (Len - S'Length + 1  .. Left_Pad'Result'Last) = S
-          and then
-   --     the "head" of the result is just padding chars
-            (for all I in 1 .. Len - S'Length =>
-                     Left_Pad'Result (I) = Pad_Char),
-   --  if not, the result is equal to the input string [S]
-        others => Left_Pad'Result = S);
+   with Contract_Cases =>
+      --  if the string is shorter than the desired length ...
+     (Len > S'Length =>
+        --  length of the result is [Len]
+        Left_Pad'Result'Length = Len and then
+        --  and looking at all characters in the result ...
+        (for all I in Left_Pad'Result'Range =>
+           Left_Pad'Result (I) =
+              --  characters before those from [S] are equal to [Pad_Char]
+             (if I <= Len - S'Length then
+                Pad_Char
+              --  remaining characters are those from [S]
+              else
+                S (I - (Len - S'Length + 1) + S'First))),
+      --  if not, the result is equal to the input string [S]
+      others => Left_Pad'Result = S);
 
 end Padding;
