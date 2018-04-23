0b08e0f58e57dbd9a085039a62c439e50255a03e
Johannes Kanig <kanig@adacore.com>
2018-04-23 10:02:55 +0900

    adding comments

--- padding.adb
+++ padding.adb
@@ -4,6 +4,9 @@ package body Padding with SPARK_Mode is
                       return String
    is
    begin
+      -- (Len - S'Length) is the number of required padding chars. We create a
+      -- string of that length with only Pad_Chars, and then concatenate this
+      -- with S.
       return (1 .. Len - S'Length => Pad_Char) & S;
    end Left_Pad;
 
--- padding.ads
+++ padding.ads
@@ -1,13 +1,23 @@
 package Padding with SPARK_Mode is
 
+   --  add [Pad_Char] characters to the left of the string [S] so that its
+   --  length becomes [Len]
    function Left_Pad (S : String; Pad_Char : Character; Len : Natural)
                       return String
+   --  This precondition seems to be needed to express the contract; The
+   --  result length should be just one less than maximal string size.
      with Pre => Len < Natural'Last,
+   --  express the specification by cases
      Contract_Cases =>
+   --  if the string is shorter than the desired length ...
        (Len > S'Length =>
-          Left_Pad'Result (Len - S'Length + 1  .. Left_Pad'Result'Last) = S
-        and then
-          (for all I in 1 .. Len - S'Length => Left_Pad'Result (I) = Pad_Char),
+   --     the "tail" of the result is equal to [S]
+            Left_Pad'Result (Len - S'Length + 1  .. Left_Pad'Result'Last) = S
+          and then
+   --     the "head" of the result is just padding chars
+            (for all I in 1 .. Len - S'Length =>
+                     Left_Pad'Result (I) = Pad_Char),
+   --  if not, the result is equal to the input string [S]
         others => Left_Pad'Result = S);
 
 end Padding;
